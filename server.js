57c2b192a8ecaeb1199bb7432c05fa016d5e014f
Richard Ma <rma@imsa.edu>
2020-08-15 21:15:31 -0500

    cors5

--- server.js
+++ server.js
@@ -8,34 +8,15 @@ const axios = require("axios");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-app.use(cors());
-app.options('*', cors());
 app.use(bodyParser.json());
-/*
 app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "*");
-
-  // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-
-  // Request headers you wish to allow
-  res.setHeader(
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000/"); // update to match the domain you will make the request from
+  res.header(
     "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
+    "Origin, X-Requested-With, Content-Type, Accept"
   );
-*/
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true);
-
-  // Pass to next layer of middleware
   next();
 });
-
 
 mongoose.connect(
   "mongodb+srv://rma2002:alexandersam29@cluster0-omtan.mongodb.net/osuproject?retryWrites=true&w=majority",
